404f91db607ea3099526c8f6a61da9c4f4c17f70
retrolous <lukaspetkunas07@gmail.com>
2024-08-07 21:56:30 +0300

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let regex = /[A-Za-z0-9]+/i;
+    filteredString = string.replace(/[^a-zA-Z0-9]/g, '');
+    for(a = 0, b = filteredString.length - 1; a < b; a++, b--){
+        if(filteredString[a].toLowerCase() !== filteredString[b].toLowerCase()){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
