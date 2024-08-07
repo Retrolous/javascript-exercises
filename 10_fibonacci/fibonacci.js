a64d53f7bf6db2a22c5a785a53f63538dde64509
retrolous <lukaspetkunas07@gmail.com>
2024-08-07 22:18:30 +0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  if (+input === 0) {
+    return 0;
+  } else if (+input < 0) {
+    return "OOPS";
+  }
 
+  let a = 1;
+  let b = 0;
+  let temp = 0;
+  for (i = 0; i < +input - 1; i++) {
+    temp = a;
+    a += b;
+    b = temp;
+  }
+  return a;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
