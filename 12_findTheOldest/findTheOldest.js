f51e5c2176a583eb830971704f71feef378edeb4
retrolous <lukaspetkunas07@gmail.com>
2024-08-07 23:00:00 +0300

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr
+    .map((input) => [
+      input.name,
+      +("yearOfDeath" in input
+        ? input.yearOfDeath
+        : +new Date().getFullYear()) - +input.yearOfBirth,
+    ])
+    .sort((a, b) => a[1] - b[1])
+    .map((input) => ({ name: input[0], age: input[1] }))[arr.length - 1];
 };
 
+console.log(
+  findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ])
+);
 // Do not edit below this line
 module.exports = findTheOldest;
